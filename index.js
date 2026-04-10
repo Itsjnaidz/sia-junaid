9b5535c4ebb3058d4cbd9815bf66261db894b874
Junaid Guindo <junaidguindo.202400745@gmail.com>
2026-04-10 18:53:28 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(express.static('public'));
+
 // Dataset: Static Array of Students
 // Dataset: Static Array of Students
 let students = [];
